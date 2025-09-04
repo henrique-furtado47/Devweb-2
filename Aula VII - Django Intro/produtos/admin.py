06ada6453ae4f270706b46ee64132bac2004d98d
Henrique <henrique.furtadoifc@gmail.com>
2025-09-04 09:40:17 -0300

    Adicionado produtos

--- Aula VII - Django Intro/produtos/admin.py
+++ Aula VII - Django Intro/produtos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Produto
+
+
 # Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ("id", "nome", "preco", "estoque", "categoria", "criado_em", "atualizado_em")
+    search_fields = ("nome",)
+    list_filter = ("criado_em", "atualizado_em")
+
--- Aula VII - Django Intro/produtos/models.py
+++ Aula VII - Django Intro/produtos/models.py
@@ -4,11 +4,12 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
+    categoria = models.CharField(max_length=50)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=8, decimal_places=2)
     estoque = models.IntegerField(default=0)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
     
-    def ___str___(self):
-        return self.nome
+    def __str__(self):
+        return f'({self.id}) {self.nome} | ({self.estoque}) | R${self.preco} | Total R${self.preco * self.estoque}'
