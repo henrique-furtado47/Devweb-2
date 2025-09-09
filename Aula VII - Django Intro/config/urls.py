1b9e50b9b7af3d81cea494a17694134078fa1749
Henrique <henrique.furtadoifc@gmail.com>
2025-09-09 10:51:22 -0300

    Criado nova url

--- Aula VII - Django Intro/config/urls.py
+++ Aula VII - Django Intro/config/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from produtos.views import ProdutoViewSet
+from rest_framework.routers import DefaultRouter
 
-from produtos.views import home, sobre
-
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
+    path('api/', include(router.urls))
 ]
-
 
