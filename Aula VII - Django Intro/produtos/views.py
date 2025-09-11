36b8184cd60a9893b7a021940d00e439d86c12ca
Henrique <henrique.furtadoifc@gmail.com>
2025-09-11 08:52:18 -0300

    Implementado filtros de buscas e ordenação

--- Aula VII - Django Intro/produtos/views.py
+++ Aula VII - Django Intro/produtos/views.py
@@ -1,4 +1,6 @@
 from django.db.models import Model
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Categoria, Produto
@@ -8,6 +10,12 @@ from .serializers import CategoriaSerializer, ProdutoSerializer
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['preco', 'estoque']  # Filtragem exata por preço e estoque
+    search_fields = ['nome']  # Busca textual
+    ordering_fields = ['nome', 'preco']  # Ordenação
+    ordering = ['id']  # Ordenação padrão
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
