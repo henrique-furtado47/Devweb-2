13f5f6b063f2bdcf03389930e7c2dd9e8e91eca6
Henrique <henrique.furtadoifc@gmail.com>
2025-09-16 11:06:20 -0300

    Adicionado novos filtros

--- Aula VII - Django Intro/produtos/views.py
+++ Aula VII - Django Intro/produtos/views.py
@@ -1,8 +1,8 @@
-from django.db.models import Model
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
+from .filters import ProdutoFilter
 from .models import Categoria, Produto
 from .serializers import CategoriaSerializer, ProdutoSerializer
 
@@ -10,12 +10,15 @@ from .serializers import CategoriaSerializer, ProdutoSerializer
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['preco', 'estoque', 'categoria__nome']  # Filtragem exata por preço e estoque
-    search_fields = ['nome']  # Busca textual
-    ordering_fields = ['nome', 'preco']  # Ordenação
-    ordering = ['id']  # Ordenação padrão
 
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProdutoFilter
+    filterset_fields = {
+        'estoque': ['exact', 'gte', 'lte']
+    }
+    search_fields = ['nome']
+    ordering_fields = ['nome', 'preco']
+    ordering = ['id']
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
